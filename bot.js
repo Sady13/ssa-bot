fa0f5c865892164106798b10ca48aa02f74dbca3
Sady13 <42273751+Sady13@users.noreply.github.com>
2018-08-14 11:29:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,4 +9,11 @@ client.on("message", (message) => {
     message.channel.send("Hola!!");
   }
 });
-client.login("NDc3ODQwNTI4MDc2MDQ2MzQ3.DlQsRw.FLYlvlYMp6_dfEiMlwSSVB5GG7o")
+client.login("NDc3ODMyODk3MjkwNTY3Njgx.DlQsRA.a34qEnwgqhmntZjqn_LoOkK6D44")
+
+client.on("guildMemberAdd", (member) => {
+   console.log(`Nuevo usuario:  ${member.user.username} se ha unido a ${member.guild.name}.`);
+   var canal = client.channels.get('123456789112455845'); 
+   canal.send(`${member.user}, bienvenido al servidor pasala bien.`);
+   
+});
